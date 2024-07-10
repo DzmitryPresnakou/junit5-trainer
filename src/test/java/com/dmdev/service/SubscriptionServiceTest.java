c4b4c5c2cf7db7aafcc0abd53f3c9b06668a35b8
DzmitryPresnakou <7745834@gmail.com>
2024-07-10 13:46:49 +0300

    added working test cancelSuccess() & void expireSuccess() to SubscriptionServiceTest

--- src/test/java/com/dmdev/service/SubscriptionServiceTest.java
+++ src/test/java/com/dmdev/service/SubscriptionServiceTest.java
@@ -7,23 +7,23 @@ import com.dmdev.entity.Status;
 import com.dmdev.entity.Subscription;
 import com.dmdev.mapper.CreateSubscriptionMapper;
 import com.dmdev.validator.CreateSubscriptionValidator;
-import com.dmdev.validator.Error;
 import com.dmdev.validator.ValidationResult;
-import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.*;
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
-
 
 @ExtendWith(MockitoExtension.class)
 class SubscriptionServiceTest {
@@ -43,17 +43,16 @@ class SubscriptionServiceTest {
 
     @Test
     void upsertSuccess() {
-
-        Subscription subscription = getSubscription(1);
+        Subscription subscription = getSubscription(1, Status.ACTIVE);
         CreateSubscriptionDto subscriptionDto = getCreateSubscriptionDto(1);
 
         List<Subscription> subscriptionList = new ArrayList<>();
         subscriptionList.add(subscription);
 
+        doReturn(new ValidationResult()).when(createSubscriptionValidator).validate(subscriptionDto);
         lenient().when(createSubscriptionMapper.map(subscriptionDto)).thenReturn(subscription);
         doReturn(subscriptionList).when(subscriptionDao).findByUserId(subscriptionDto.getUserId());
         doReturn(subscription).when(subscriptionDao).upsert(subscription);
-        doReturn(new ValidationResult()).when(createSubscriptionValidator).validate(subscriptionDto);
 
         Subscription actualResult = subscriptionService.upsert(subscriptionDto);
 
@@ -61,30 +60,10 @@ class SubscriptionServiceTest {
         assertThat(actualResult).isEqualTo(subscription);
     }
 
-    private static CreateSubscriptionDto getCreateSubscriptionDto(Integer userId) {
-        return CreateSubscriptionDto.builder()
-                .userId(userId)
-                .name("firstSubscription")
-                .provider(Provider.APPLE.name())
-                .expirationDate(EXPIRATION_DATE.toInstant(ZoneOffset.UTC))
-                .build();
-    }
-
-    private static Subscription getSubscription(Integer userId) {
-        return Subscription.builder()
-                .id(1)
-                .userId(userId)
-                .name("firstSubscription")
-                .provider(Provider.APPLE)
-                .expirationDate(EXPIRATION_DATE.toInstant(ZoneOffset.UTC))
-                .status(Status.ACTIVE)
-                .build();
-    }
 
     @Test
     void upsertFailed() {
-
-        Subscription subscription = getSubscription(null);
+        Subscription subscription = getSubscription(null, Status.ACTIVE);
         CreateSubscriptionDto subscriptionDto = getCreateSubscriptionDto(null);
 
         List<Subscription> subscriptionList = new ArrayList<>();
@@ -97,5 +76,46 @@ class SubscriptionServiceTest {
 
         assertThat(actualResult).isNull();
         verifyNoInteractions(createSubscriptionMapper);
+    }
+
+    @Test
+    void cancelSuccess() {
+        Subscription subscription = getSubscription(1, Status.ACTIVE);
+
+        doReturn(Optional.of(subscription)).when(subscriptionDao).findById(subscription.getId());
+        subscriptionService.cancel(subscription.getId());
+
+        verify(subscriptionDao, times(1)).update(subscription);
+
+    }
+
+    @Test
+    void expireSuccess() {
+        Subscription subscription = getSubscription(1, Status.ACTIVE);
+
+        doReturn(Optional.of(subscription)).when(subscriptionDao).findById(subscription.getId());
+        subscriptionService.expire(subscription.getId());
+
+        verify(subscriptionDao, times(1)).update(subscription);
+    }
+
+    private static CreateSubscriptionDto getCreateSubscriptionDto(Integer userId) {
+        return CreateSubscriptionDto.builder()
+                .userId(userId)
+                .name("firstSubscription")
+                .provider(Provider.APPLE.name())
+                .expirationDate(EXPIRATION_DATE.toInstant(ZoneOffset.UTC))
+                .build();
+    }
+
+    private static Subscription getSubscription(Integer userId, Status status) {
+        return Subscription.builder()
+                .id(1)
+                .userId(userId)
+                .name("firstSubscription")
+                .provider(Provider.APPLE)
+                .expirationDate(EXPIRATION_DATE.toInstant(ZoneOffset.UTC))
+                .status(status)
+                .build();
     }
 }
